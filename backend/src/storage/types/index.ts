fd9bb6331c9d7c8f6cd287f4dd1baeda70d200f2
Roman Borovik <starmordar@gmail.com>
2024-08-29 16:41:12 +0300

    Get rid of NestJS as backend

--- backend/src/storage/types/index.ts
+++
@@ -1,8 +0,0 @@
-export interface Storage {
-  /**
-   * Store an attachment
-   * @param attachment The attachment to store
-   * @returns The uploaded file name
-   */
-  upload(filename: string, buffer: Buffer): Promise<string>;
-}
