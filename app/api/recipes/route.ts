ea34183daf13c0f84742cefd8ad9ebdf28a345b8
Roman Borovik <starmordar@gmail.com>
2024-09-06 16:25:13 +0300

    Remove console.log

--- app/api/recipes/route.ts
+++ app/api/recipes/route.ts
@@ -4,8 +4,6 @@ import { prisma } from '@/prisma/prisma-client';
 
 export async function GET(req: NextRequest) {
   const query = req.nextUrl.searchParams.get('search') ?? '';
-
-  console.log('query :>> ', query);
   const recipes = prisma.recipe.findMany();
 
   return NextResponse.json(recipes);
