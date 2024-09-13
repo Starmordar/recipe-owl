84de67266fb6698b1bdf0a3c79234fae02e2129d
Roman Borovik <starmordar@gmail.com>
2024-09-13 14:56:47 +0300

    Update redirect email

--- lib/lucia/oauth.ts
+++ lib/lucia/oauth.ts
@@ -3,7 +3,7 @@ import { Google } from 'arctic';
 const google = new Google(
   process.env.AUTH_GOOGLE_ID,
   process.env.AUTH_GOOGLE_SECRET,
-  'http://localhost:3000/api/auth/google/callback',
+  process.env.AUTH_GOOGLE_REDIRECT_URL,
 );
 
 const scopes = [
