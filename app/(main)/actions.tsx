5d7972180d1062c4ea363d6fdea2084954c0c3b6
Roman Borovik <starmordar@gmail.com>
2024-10-07 20:43:16 +0300

    hotfix

--- app/(main)/actions.tsx
+++ app/(main)/actions.tsx
@@ -3,7 +3,6 @@ import { RecipeDetails } from '@/types/api';
 
 async function getTodaysRecipe(): Promise<RecipeDetails | null> {
   const recipe = await prisma.recipe.findFirst({
-    where: { id: 2 },
     include: { ingredients: { orderBy: { order: 'asc' } }, user: true },
   });
 
