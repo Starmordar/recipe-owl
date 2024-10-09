e1fbce6463d708df848063370d230fb1fefe2161
Roman Borovik <starmordar@gmail.com>
2024-10-09 18:07:32 +0300

    add cron validation check

--- app/api/recipes/change-of-the-day/route.ts
+++ app/api/recipes/change-of-the-day/route.ts
@@ -3,10 +3,10 @@ import { NextRequest, NextResponse } from 'next/server';
 import { createRecipeOfTheDay } from '@/app/(main)/actions';
 
 export async function GET(req: NextRequest) {
-  // const authHeader = req.headers.get('authorization');
-  // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-  //   return new NextResponse('Unauthorized', { status: 401 });
-  // }
+  const authHeader = req.headers.get('authorization');
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new NextResponse('Unauthorized', { status: 401 });
+  }
 
   await createRecipeOfTheDay();
 
