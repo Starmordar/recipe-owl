e41f8a7189217e1d397b1485329acfc93ad06735
Roman Borovik <starmordar@gmail.com>
2024-10-10 10:11:30 +0300

    Add link to the recipe of the day

--- app/(main)/_components/recipe-of-the-day/components/recipe-info.tsx
+++ app/(main)/_components/recipe-of-the-day/components/recipe-info.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import Link from 'next/link';
 import { forwardRef } from 'react';
 
 import RecipeAuthor from '@/components/recipe-details/components/recipe-author';
+import { publicUrls } from '@/config/url';
 
 import type { RecipePreview } from '@/types/api';
 
@@ -12,15 +14,17 @@ interface Props {
 
 const RecipeInfo = forwardRef<HTMLDivElement, Props>(({ recipe }, ref) => {
   return (
-    <article
-      ref={ref}
-      className='transform -translate-y-10 -mb-6 mx-auto flex flex-col p-4 w-[80vw] rounded-lg bg-secondary z-100'
-    >
-      <span className='text-base'>Recipe of the Day</span>
-      <h1 className='text-2xl font-semibold mb-3'>{recipe.title}</h1>
+    <Link href={publicUrls.recipe(recipe.id)} aria-label='Read more about Recipe of the Day'>
+      <article
+        ref={ref}
+        className='transform -translate-y-10 -mb-6 mx-auto flex flex-col p-4 w-[80vw] rounded-lg bg-secondary z-100'
+      >
+        <span className='text-base'>Recipe of the Day</span>
+        <h1 className='text-2xl font-semibold mb-3'>{recipe.title}</h1>
 
-      <RecipeAuthor recipe={recipe} avatarSize={20} />
-    </article>
+        <RecipeAuthor recipe={recipe} avatarSize={20} />
+      </article>
+    </Link>
   );
 });
 
