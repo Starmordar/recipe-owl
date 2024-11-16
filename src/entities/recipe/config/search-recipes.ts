5f265d9e5fcfcb9f286feee2906a9371dc5e58a8
Roman Borovik <starmordar@gmail.com>
2024-11-16 10:19:33 +0300

    Adjust categories list

--- src/entities/recipe/config/search-recipes.ts
+++ src/entities/recipe/config/search-recipes.ts
@@ -1,7 +1,7 @@
 const ingredientsCategory = 'Ingredients';
 const onlySavedCategory = 'Only Saved';
 
-const tagsCategories = ['Mood', 'Cuisine', 'Nutrition', 'Main Ingredient', 'Complexity', 'Time'];
+const tagsCategories = ['Mood', 'Cuisine', 'Nutrition', 'Complexity', 'Time'];
 
 const quickFilterCategories = [ingredientsCategory, ...tagsCategories];
 const filterCategories = [ingredientsCategory, onlySavedCategory, ...tagsCategories];
