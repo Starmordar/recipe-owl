1c7faeb8a15949fea7d5ed72b9022490ca7156b2
Roman Borovik <starmordar@gmail.com>
2024-11-19 11:20:34 +0300

    Update page skeleton

--- src/views/recipes/ui/skeleton/index.tsx
+++ src/views/recipes/ui/skeleton/index.tsx
@@ -1,19 +1,30 @@
 import { Skeleton } from '@/src/shared/ui/skeleton';
 
 function RecipesPageSkeleton() {
-  const cards = new Array<number>(20).fill(0);
+  const cards = new Array<number>(10).fill(0);
 
   return (
-    <div className='grid grid-cols-2 md:grid-cols-3 gap-x-3 gap-y-4'>
-      {cards.map((_, index) => (
-        <div key={index} className='flex flex-col gap-2'>
-          <Skeleton className='h-[20vh] rounded-lg' />
-          <div className='space-y-2'>
-            <Skeleton className='h-5' />
-          </div>
+    <>
+      <div className='px-4 py-2 space-y-2'>
+        <Skeleton className='h-8' />
+        <div className='flex gap-x-2'>
+          <Skeleton className='h-6 w-24' />
+          <Skeleton className='h-6 w-52' />
         </div>
-      ))}
-    </div>
+      </div>
+      <main className='page-container pt-2'>
+        <div className='grid grid-cols-2 md:grid-cols-3 gap-x-3 gap-y-4'>
+          {cards.map((_, index) => (
+            <div key={index} className='flex flex-col gap-2'>
+              <Skeleton className='h-[25vh] rounded-lg' />
+              <div className='space-y-2'>
+                <Skeleton className='h-5' />
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </>
   );
 }
 
